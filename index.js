5218e67510c33afc80eb22cd438a660f9d268e61
PaulSebi <paulppsebastian@gmail.com>
2017-11-14 01:21:09 +0530

    After first deploy

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const fs = require('fs')
-const _ = require('underscore') 
+const _ = require('underscore')
 
 try{
-    let config = JSON.parse(fs.readFileSync('snerrfile.json')).config
+    const config = JSON.parse(fs.readFileSync('./snerrfile.json')).config
     _.each(config, (con) => {
         if(!(typeof con.pre=='string' && typeof con.post=='string' && typeof con.length == 'number' && typeof con.skip == 'boolean'))
             return console.error('SN101FE')
-    })    
+    })
 }
 catch(e){
     return console.error('SN101NF')
@@ -17,26 +17,25 @@ catch(e){
 module.exports = {
     err : (snerr, err, addMessage) => {
         let file
+        console.log('here')
         try{
-            file = JSON.parse(fs.readFileSync('snerrfile.json'))
+            file = JSON.parse(fs.readFileSync('./snerrfile.json'))
         }
         catch(e){
             return console.error('SN101NF')
         }
-        
+
         const config = file.config
         let code = file.code, message = ""
-        const error = {code: snerr,err: err}
-        if(addMessage) //flag check
-            error.message = message
-        
+        let error = {code: snerr,err: err, message: null}
+
         //cycle through all levels of config [SN] [4] [01] [NF]
         try{
             _.each(config, (con) => {
                 //break down the snerr code into json keys
                 const key = snerr.substring(0, con.length)
                 snerr = snerr.substring(con.length)
-                
+
                 //if key does not exist in code, add key
                 if(!code[key])
                     code[key] = {"is": ""}
@@ -44,14 +43,16 @@ module.exports = {
 
                 //to add decoded message to error object (will only work once snerrfile has been updated with meaning)
                 if(addMessage)
-                    message += con. skip? "": (con.pre + code.is + con.post)
+                    message += con.skip? "": (con.pre + code.is + con.post)
             })
+            if(addMessage) //flag check
+                error.message = message
         }
         catch(e){
             return console.error('SN101FE')
         }
         try{
-            return fs.writeFileSync('snerrfile.json', JSON.stringify(file, null, 2))
+            fs.writeFileSync('./snerrfile.json', JSON.stringify(file, null, 2))
         }
         catch(e){
             console.error('SN101NF')
@@ -61,7 +62,7 @@ module.exports = {
     show : (snerr) => {
         let file
         try{
-            file = JSON.parse(fs.readFileSync('snerrfile.json'))
+            file = JSON.parse(fs.readFileSync('./snerrfile.json'))
         }
         catch(e){
             return console.error('SN101NF')
